9d77148f945121fa476c9aecf43b75a767bdc3f4
nolanzhong <nolan.zhong@duke.edu>
2023-01-09 12:18:12 -0500

    added images

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -16,6 +16,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tabView.delegate = self
         tabView.dataSource = self
         
+        var rideNames = [String]()
+        rideNames.append("Hagrids Motorbike Adventure")
+        rideNames.append("Velocicoaster")
+        rideNames.append("Revenge of the Mummy")
+        rideNames.append("Escape from Gringotts")
+        rideNames.append("Spiderman Adventure")
+        
+        var rideImages = [UIImage]()
+        rideImages.append(UIImage(named: "hagridsmotorbike.jpeg")!)
+        rideImages.append(UIImage(named: "velocicoaster.jpeg")!)
+        rideImages.append(UIImage(named: "revenge of mummy.jpeg")!)
+        rideImages.append(UIImage(named: "gringotts.jpeg")!)
+        rideImages.append(UIImage(named: "spidermanride.jpeg")!)
+        
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
