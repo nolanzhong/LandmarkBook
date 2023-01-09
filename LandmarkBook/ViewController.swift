83fd841c92b806afaa1db08164c00632b0aada81
nolanzhong <nolan.zhong@duke.edu>
2023-01-09 12:03:14 -0500

    configured table view

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tabView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tabView.delegate = self
+        tabView.dataSource = self
+        
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // number of rows
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        //cell.textLabel?.text = "test"
+        var content = cell.defaultContentConfiguration() // define what have to show in cell
+        content.text = "TableView"
+        content.secondaryText = "test"
+        cell.contentConfiguration = content
+        return cell
+    }
 }
 
