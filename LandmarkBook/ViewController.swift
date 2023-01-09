efc8c8ee04c8d2bd4a80bffc9873fa610e1668b6
nolanzhong <nolan.zhong@duke.edu>
2023-01-09 12:22:54 -0500

    added in tableview labels

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -10,20 +10,23 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tabView: UITableView!
+    
+    var rideNames = [String]()
+    var rideImages = [UIImage]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tabView.delegate = self
         tabView.dataSource = self
         
-        var rideNames = [String]()
+        
         rideNames.append("Hagrids Motorbike Adventure")
         rideNames.append("Velocicoaster")
         rideNames.append("Revenge of the Mummy")
         rideNames.append("Escape from Gringotts")
         rideNames.append("Spiderman Adventure")
         
-        var rideImages = [UIImage]()
+        
         rideImages.append(UIImage(named: "hagridsmotorbike.jpeg")!)
         rideImages.append(UIImage(named: "velocicoaster.jpeg")!)
         rideImages.append(UIImage(named: "revenge of mummy.jpeg")!)
@@ -34,14 +37,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // number of rows
-        return 10
+        return rideNames.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         //cell.textLabel?.text = "test"
         var content = cell.defaultContentConfiguration() // define what have to show in cell
-        content.text = "TableView"
-        content.secondaryText = "test"
+        content.text = rideNames[indexPath.row] //displays corresponding index in ridename
         cell.contentConfiguration = content
         return cell
     }
